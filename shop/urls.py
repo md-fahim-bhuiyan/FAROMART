dbd0dee106e90dc82f81fc3673b4904977a1058a
Md. Fahim Bhuiyan <67233830+md-fahim-bhuiyan@users.noreply.github.com>
2022-09-22 20:57:08 +0600

    new2

--- shop/urls.py
+++ shop/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path("", views.index, name="ShopHome"),
     path("about/", views.about, name="AboutUs"),
     path("contact/", views.contact, name="ContactUs"),
-    # path("tracker/", views.tracker, name="TrackerStatus"),
-    # path("search/", views.search, name="Search"),
-    # path("productview/", views.productview, name="ProductView"),
-    # path("checkout/", views.checkout, name="CheckOut"),
-]
+    path("tracker/", views.tracker, name="TrackerStatus"),
+    path("search/", views.search, name="Search"),
+    path("productview/", views.productview, name="ProductView"),
+    path("checkout/", views.checkout, name="CheckOut"),
+ ]
--- shop/views.py
+++ shop/views.py
@@ -9,3 +9,15 @@ def about(request):
 
 def contact(request):
    return render(request, 'shop/contact/index.html')
+
+def tracker(request):
+    return HttpResponse("We are at tracker")
+
+def search(request):
+    return HttpResponse("We are at search")
+
+def productview(request):
+    return HttpResponse("We are at product view")
+
+def checkout(request):
+    return HttpResponse("We are at checkout")
